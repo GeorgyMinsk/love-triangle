f2688c33b3bb1fa22848a73e60897b9f70174e6d
Georhi Buklaga <buklaga.98@yandex.ru>
2018-09-11 13:17:43 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+	var n=0;
+	for(var i=0 ; i < preferences.length ; i++ )
+	{
+		 if (preferences[preferences[preferences[i]-1]-1]-1==i && i+1!=preferences[i])
+             n++;
+	}
+	return n/3;
   // your implementation
 };
